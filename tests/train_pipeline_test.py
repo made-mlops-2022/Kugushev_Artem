0dbe5f11e7fc3b304ad25d6469ff11af024ba9fa
artemkugushev <mr.ar4ik33@gmail.com>
2022-11-06 10:29:24 +0300

    Add testing train pipeline

--- tests/train_pipeline_test.py
+++ tests/train_pipeline_test.py
@@ -1,16 +1,18 @@
 """
 Class providing tests for train_pipeline
 """
-import csv
-import os
+
+
 from unittest import TestCase
 
-import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 
-from data.dataclass.predict_pipeline_params import read_predict_pipeline_params
 from data.dataclass.test_params import read_test_params
-from predict_pipeline import get_model_path, save_preds_to_file
 from logs.create_logger import create_logger
+from models import create_inference_pipeline
+from train_pipeline import split_x_y
 
 PATH_TO_CONFIG = "configs/test_config.yaml"
 
@@ -25,14 +27,28 @@ class TrainPipelineTest(TestCase):
 
     def test_split_x_y(self):
         """
-        Assert that model path is correct
+        Assert that data splits to X and Y correctly
         :return: None
         """
-        pass
+
+        logger.info("Start testing train pipeline")
+        test_df = pd.DataFrame(data={"feature": [23, 45, 11], "age": [45, 21, 34], "condition": [1, 0, 1]})
+        test_features, test_target = split_x_y(test_df, "condition")
+        self.assertEqual(test_features.shape, (3, 2))
+        self.assertEqual(test_target.shape, (3, ))
+        self.assertEqual(test_target.array, [1, 0, 1])
+        logger.info("Finish testing train pipeline")
+
 
     def test_create_pipeline(self):
         """
         Assert that pipeline created
         :return: None
         """
-        pass
+
+        logger.info("Start test pipeline creation")
+        model = LogisticRegression()
+        pipeline = create_inference_pipeline(model)
+        self.assertIsInstance(pipeline, Pipeline)
+        self.assertEqual(pipeline.steps, [("model_part",  model)])
+        logger.info("Finish test pipeline creation")
